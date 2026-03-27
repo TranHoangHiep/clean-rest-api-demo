fa28ddc3b52376637cc0d474a31703273247c170
Hiep Tran Hoang <hiep.tranhoang@ascendcorp.com>
2026-03-27 14:01:11 +0700

    First commit

---
+++ src/main/java/com/hoanghiep/cleanrestapidemo/CleanRestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hoanghiep.cleanrestapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanRestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanRestApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoanghiep/cleanrestapidemo/CleanRestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanghiep.cleanrestapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanRestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
